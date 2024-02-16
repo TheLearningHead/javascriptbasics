286503c9ce06d5d78f46f974b1898b0936d76fd1
Ayush Agrawal <ayushagrawal1811@gmail.com>
2024-02-16 23:02:48 +0530

    Learnt loops associated with arrays

---
+++ 06_loops_with_arrays.js
@@ -0,0 +1,12 @@
+let num = [1, 2, 3, 4, 5];
+
+// Basic for loop
+for (let i = 0; i < num.length; i++) {
+    console.log(num[i]);
+}
+
+// forEach loop
+num.forEach((element)=>{
+    console.log(element**2);
+})
+
