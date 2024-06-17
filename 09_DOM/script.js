516873ed46f23366eeb2cc8420cdba05cd13b955
Ayush Agrawal <ayushagrawal1811@gmail.com>
2024-06-17 19:29:04 +0530

    More about DOM

--- 09_DOM/script.js
+++ 09_DOM/script.js
@@ -1,9 +1,11 @@
 // window object represents the browser window and provides methods to control it. It is a global object
 console.log(window)
 
-// DOM is Document Object Model. It represents the page contents as HTML
-console.log(document.body)
+// DOM is Document Object Model. It represents the page contents as HTML or we can say that it displays the entire content of the page as a document tree.
+console.log(document.body) // displays the contents of the body of the page
 document.body.style.background = "red"
+document.title = "Welcome to concepts of DOM!"
 
 // BOM is Browser Object Model. It represents the additional objects provided byt he host environment(browser) for working with everything except the document
-location.href = "https://www.google.com"
+// location.href = "https://www.google.com"
+
