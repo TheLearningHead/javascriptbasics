a808d655630333bcaaf995911b31d485b173058a
Ayush Agrawal <ayushagrawal1811@gmail.com>
2024-06-17 22:06:56 +0530

    More about DOM

--- 09_DOM/script.js
+++ 09_DOM/script.js
@@ -3,8 +3,8 @@ console.log(window)
 
 // DOM is Document Object Model. It represents the page contents as HTML or we can say that it displays the entire content of the page as a document tree.
 console.log(document.body) // displays the contents of the body of the page
-document.body.style.background = "red"
-document.title = "Welcome to concepts of DOM!"
+document.body.style.backgroundColor = "red" // adds an inline css to the body changing its background color to the color specified
+document.title = "Welcome to concepts of DOM!" // changes the title of the page
 
 // BOM is Browser Object Model. It represents the additional objects provided byt he host environment(browser) for working with everything except the document
 // location.href = "https://www.google.com"
